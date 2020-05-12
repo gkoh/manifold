a3f2e20bafdf6a22e738de307ada192d3385aded
Guo-Rong Koh <guorong.koh@gmail.com>
2020-05-12 13:46:37 +0930

    Blacken the code with: ``` black --check --diff . black . ```

--- manifold/manifold.py
+++ manifold/manifold.py
@@ -1,15 +1,15 @@
 # encoding: utf-8
 
-'''manifold
+"""manifold
 
 An SMF service manifest creation tool.
-'''
+"""
 
-__author__ = 'Chris Miles'
-__copyright__ = '(c) Chris Miles 2008. All rights reserved.'
-__license__ = 'GPL http://www.gnu.org/licenses/gpl.txt'
-__id__ = '$Id$'
-__url__ = '$URL$'
+__author__ = "Chris Miles"
+__copyright__ = "(c) Chris Miles 2008. All rights reserved."
+__license__ = "GPL http://www.gnu.org/licenses/gpl.txt"
+__id__ = "$Id$"
+__url__ = "$URL$"
 
 
 # ---- Imports ----
@@ -141,39 +141,47 @@ MANIFEST_TEMPLATE = """<?xml version="1.0"?>
 """
 
 
-
 # ---- Classes ----
 
+
 class CONFIG_BASE(object):
-    def __init__(self, name, require_value=False, default=None, description=None, example=None, accepted_values=None):
+    def __init__(
+        self,
+        name,
+        require_value=False,
+        default=None,
+        description=None,
+        example=None,
+        accepted_values=None,
+    ):
         self.name = name
         self.require_value = require_value
         self.default = default
         self.description = description
         self.example = example
         self.accepted_values = accepted_values
-    
+
     def prompt(self):
         raise NotImplemented()
-    
+
     def ask(self, config):
         raise NotImplemented()
-    
+
 
 class CONFIG_STR(CONFIG_BASE):
     def prompt(self):
         if self.description:
             s = self.description
         else:
-            s = "Enter value for %s" %self.name
+            s = "Enter value for %s" % self.name
         if self.example:
-            s += " (example: %s)" %self.example
+            s += " (example: %s)" % self.example
         if self.default:
-            s += " [%s] "% self.default
+            s += " [%s] " % self.default
         else:
             s += " [] "
         return s
-    
+
     def ask(self, config):
         r = None
         while r is None or (self.require_value and not r):
@@ -181,104 +189,95 @@ class CONFIG_STR(CONFIG_BASE):
             if not r and self.default is not None:
                 r = self.default
             elif self.accepted_values and r not in self.accepted_values:
-                print("Sorry, you must enter one of: " + ', '.join(['"{}"'.format(s) for s in self.accepted_values]))
+                print(
+                    "Sorry, you must enter one of: "
+                    + ", ".join(['"{}"'.format(s) for s in self.accepted_values])
+                )
                 r = None
         if not r:
             r = None
         return r
-    
+
 
 class CONFIG_BOOL(CONFIG_BASE):
     def prompt(self):
         if self.description:
             s = self.description
         else:
-            s = "%s" %self.name
+            s = "%s" % self.name
         s += " (yes/no)"
         if self.default is not None:
             if self.default:
                 default = "yes"
             else:
                 default = "no"
-            s += " [%s]"% default
+            s += " [%s]" % default
         s += " ? "
         return s
-    
+
     def ask(self, config):
-        answers = dict(
-            yes = True,
-            ye = True,
-            y = True,
-            no = False,
-            n = False
-        )
+        answers = dict(yes=True, ye=True, y=True, no=False, n=False)
         r = None
-        while not r in answers.keys() and r != '':
+        while not r in answers.keys() and r != "":
             r = input(self.prompt()).strip().lower()
-        
+
         if r:
             r = answers[r]
         elif self.default is not None:
             r = self.default
-        
+
         if r:
-            r = 'true'
+            r = "true"
         else:
-            r = 'false'
-        
+            r = "false"
+
         return r
-    
+
 
 class CONFIG_IF(CONFIG_BASE):
     def __init__(self, *args, **kwargs):
-        self.questions = kwargs.get('questions', [])
-        del kwargs['questions']
+        self.questions = kwargs.get("questions", [])
+        del kwargs["questions"]
         super(CONFIG_IF, self).__init__(*args, **kwargs)
-    
+
     def prompt(self):
         if self.description:
             s = self.description
         else:
-            s = "%s" %self.name
+            s = "%s" % self.name
         s += " (yes/no)"
         if self.default is not None:
             if self.default:
                 default = "yes"
             else:
                 default = "no"
-            s += " [%s]"% default
+            s += " [%s]" % default
         s += " ? "
         return s
-    
+
     def ask(self, config):
-        answers = dict(
-            yes = True,
-            ye = True,
-            y = True,
-            no = False,
-            n = False
-        )
+        answers = dict(yes=True, ye=True, y=True, no=False, n=False)
         r = None
-        while not r in answers.keys() and r != '':
+        while not r in answers.keys() and r != "":
             r = input(self.prompt()).strip().lower()
-        
+
         if r:
             r = answers[r]
         elif self.default is not None:
             r = self.default
-        
+
         if r:
             # if answer to this question is "yes" then ask user extra questions
             config.update(ask_user(self.questions))
         return r
-    
 
 
 # ---- Functions ----
 
+
 def ask_user(service_questions):
     response = {}
-    
+
     for q in service_questions:
         print
         response[q.name] = q.ask(response)
@@ -288,70 +287,75 @@ def ask_user(service_questions):
 def generate_service_config():
     service_questions = [
         CONFIG_STR(
-            'service_category',
+            "service_category",
             require_value=True,
-            default='site',
-            description='The service category',
-            example="'site' or '/application/database'"
+            default="site",
+            description="The service category",
+            example="'site' or '/application/database'",
         ),
         CONFIG_STR(
-            'service_name',
+            "service_name",
             require_value=True,
             description="""The name of the service, which follows the service category
   """,
-            example="'myapp'"
+            example="'myapp'",
         ),
         CONFIG_STR(
-            'service_version',
+            "service_version",
             require_value=True,
             description="The version of the service manifest",
-            default='1',
-            example="'1'"
+            default="1",
+            example="'1'",
         ),
         CONFIG_STR(
-            'common_name',
+            "common_name",
             require_value=False,
             description="""The human readable name of the service
   """,
-            example="'My service.'"
+            example="'My service.'",
         ),
         CONFIG_IF(
-            'multi_instance',
+            "multi_instance",
             description="Can this service run multiple instances",
             default=False,
             questions=[
-                CONFIG_STR('instance_name', require_value=True, default='default', example="default")
-            ]
+                CONFIG_STR(
+                    "instance_name",
+                    require_value=True,
+                    default="default",
+                    example="default",
+                )
+            ],
         ),
         CONFIG_STR(
-            'config_file',
+            "config_file",
             require_value=False,
             description="""Full path to a config file; leave blank if no config file
   required""",
-            example="'/etc/myservice.conf'"
+            example="'/etc/myservice.conf'",
         ),
         CONFIG_STR(
-            'exec_method_start',
+            "exec_method_start",
             require_value=True,
             description="""The full command to start the service; may contain
   '%{config_file}' to substitute the configuration file
   """,
-            example="'/usr/bin/myservice %{config_file}'"
+            example="'/usr/bin/myservice %{config_file}'",
         ),
         CONFIG_STR(
-            'exec_method_stop',
+            "exec_method_stop",
             require_value=True,
-            default = ':kill',
+            default=":kill",
             description="""The full command to stop the service; may specify ':kill' to let
   SMF kill the service processes automatically
   """,
             example="""'/usr/bin/myservice_ctl stop' or ':kill' to let SMF kill
-  the service processes automatically"""
+  the service processes automatically""",
         ),
         CONFIG_STR(
-            'startd_model',
+            "startd_model",
             require_value=True,
-            default = 'wait',
+            default="wait",
             description="""Choose a process management model:
   'wait'      : long-running process that runs in the foreground (default)
   'contract'  : long-running process that daemonizes or forks itself
@@ -359,56 +363,55 @@ def generate_service_config():
   'transient' : short-lived process, performs an action and ends quickly
   """,
             # example="",
-            accepted_values = ('wait', 'contract', 'transient'),
+            accepted_values=("wait", "contract", "transient"),
         ),
         CONFIG_BOOL(
-            'depends_on_network',
+            "depends_on_network",
             description="Does this service depend on the network being ready",
-            default=True
+            default=True,
         ),
         CONFIG_BOOL(
-            'depends_on_filesystem',
+            "depends_on_filesystem",
             description="Does this service depend on the local filesystems being ready",
-            default=True
+            default=True,
         ),
         CONFIG_BOOL(
-            'instance_enabled',
+            "instance_enabled",
             default=False,
-            description="Should the service be enabled by default"
+            description="Should the service be enabled by default",
         ),
         CONFIG_STR(
-            'method_credential_user',
+            "method_credential_user",
             require_value=False,
             description="""The user to change to when executing the
   start/stop/refresh methods""",
-            example="'webservd'"
+            example="'webservd'",
         ),
         CONFIG_STR(
-            'method_credential_group',
+            "method_credential_group",
             require_value=False,
             description="""The group to change to when executing the
   start/stop/refresh methods""",
-            example="'webservd'"
+            example="'webservd'",
         ),
     ]
-    
+
     service_config = ask_user(service_questions)
     logging.debug(service_config)
-    
+
     return service_config
 
 
 def create_manifest(outfp, service_config):
     tmpl = MarkupTemplate(MANIFEST_TEMPLATE)
-    xml = tmpl.generate(**service_config).render('xml', strip_whitespace=False)
+    xml = tmpl.generate(**service_config).render("xml", strip_whitespace=False)
     outfp.write(xml)
-
 
 
 def main(argv=None):
     if argv is None:
         argv = sys.argv
-    
+
     # define usage and version messages
     usageMsg = "usage: %s [options] output.xml" % sys.argv[0]
     versionMsg = """%s %s""" % (os.path.basename(argv[0]), version)
@@ -418,50 +421,66 @@ For example, "svccfg validate myservice.xml", "svccfg -v import myservice.xml".
 """
 
     # get a parser object and define our options
-    parser = optparse.OptionParser(usage=usageMsg, version=versionMsg, description=description)
-    
+    parser = optparse.OptionParser(
+        usage=usageMsg, version=versionMsg, description=description
+    )
+
     # Switches
-    parser.add_option('-v', '--verbose', dest='verbose',
-        action='store_true', default=False,
-        help="verbose output")
-    parser.add_option('-d', '--debug', dest='debug',
-        action='store_true', default=False,
-        help="debugging output (very verbose)")
-    
+    parser.add_option(
+        "-v",
+        "--verbose",
+        dest="verbose",
+        action="store_true",
+        default=False,
+        help="verbose output",
+    )
+    parser.add_option(
+        "-d",
+        "--debug",
+        dest="debug",
+        action="store_true",
+        default=False,
+        help="debugging output (very verbose)",
+    )
+
     # Parse options & arguments
     (options, args) = parser.parse_args()
-    
+
     if len(args) < 1:
         parser.error("Output file must be specified.")
     if len(args) > 1:
         parser.error("Only one output file can be specified.")
-    
+
     if options.verbose:
         loglevel = logging.INFO
     elif options.debug:
         loglevel = logging.DEBUG
     else:
         loglevel = logging.WARNING
-    
+
     logging.basicConfig(
         level=loglevel,
         # format='%(asctime)s %(levelname)s %(message)s',
-        format='%(message)s',
+        format="%(message)s",
     )
-    
+
     output_filename = args[0]
-    output = open(output_filename, 'w')
-    
+    output = open(output_filename, "w")
+
     service_config = generate_service_config()
-    
+
     create_manifest(output, service_config)
-    
+
     output.close()
-    
+
     print("\nManifest written to {}".format(output_filename))
-    print('You can validate the XML file with "svccfg validate {}"'.format(output_filename))
+    print(
+        'You can validate the XML file with "svccfg validate {}"'.format(
+            output_filename
+        )
+    )
     print('And create the SMF service with "svccfg import {}"'.format(output_filename))
-    
+
     return 0
 
 
--- setup.py
+++ setup.py
@@ -3,14 +3,15 @@ import sys, os
 
 from manifold.release import version
 
-setup(name='Manifold',
-      version=version,
-      description="An SMF service manifest creation tool.",
-      long_description="""\
+setup(
+    name="Manifold",
+    version=version,
+    description="An SMF service manifest creation tool.",
+    long_description="""\
 A command-line tool to simplify creating custom service manifests for
 SMF on Solaris systems.
 """,
-      classifiers=[
+    classifiers=[
         "Development Status :: 4 - Beta",
         "Environment :: Console",
         "Intended Audience :: System Administrators",
@@ -19,22 +20,18 @@ SMF on Solaris systems.
         "Programming Language :: Python",
         "Topic :: System :: Systems Administration",
         "Topic :: Utilities",
-      ], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      keywords='SMF manifest XML',
-      author='Chris Miles',
-      author_email='miles.chris@gmail.com',
-      url='http://code.google.com/p/manifold/',
-      license='MIT',
-      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
-      include_package_data=True,
-      zip_safe=True,
-      install_requires=[
-          # -*- Extra requirements: -*-
-          "Genshi",
-      ],
-      entry_points = {
-          'console_scripts': [
-              'manifold = manifold.manifold:main',
-          ],
-      },
+    ],  # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    keywords="SMF manifest XML",
+    author="Chris Miles",
+    author_email="miles.chris@gmail.com",
+    url="http://code.google.com/p/manifold/",
+    license="MIT",
+    packages=find_packages(exclude=["ez_setup", "examples", "tests"]),
+    include_package_data=True,
+    zip_safe=True,
+    install_requires=[
+        # -*- Extra requirements: -*-
+        "Genshi",
+    ],
+    entry_points={"console_scripts": ["manifold = manifold.manifold:main",],},
 )
